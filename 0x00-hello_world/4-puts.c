083cdcc28d7f4cabc102e6b0821043796e32818c
el hihizakarya <zakarya.elhihi@gmail.com>
2023-03-16 14:40:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main -print a line of code using puts
+ * main -print "Programming is like building a multilingual puzzle
  *
- * Retun: 0
+ * Return: Always 0
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
