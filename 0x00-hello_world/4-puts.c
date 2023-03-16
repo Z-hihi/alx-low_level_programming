5b0728600c5ed61fd19005b76d0b8aa7053ddcb7
el hihizakarya <zakarya.elhihi@gmail.com>
2023-03-16 06:06:41 -0700

    progrmme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * The main function of the program.
+ *
+ * This function prints the string "Hello, world!" to the console
+ * using the `puts` function.
+ *
+ * @return 0 indicating success
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
