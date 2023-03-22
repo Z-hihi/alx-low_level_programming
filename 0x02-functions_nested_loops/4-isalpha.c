cd50debe522f3eac09105659ab02f7d2c63b9b46
el hihizakarya <zakarya.elhihi@gmail.com>
2023-03-22 02:11:35 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - cheks the input is caractere
+ *
+ * @c: input of function
+ * Return: 1 if c true 0 else
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 $$ c <= 122)
+		return (1);
+	if (c >= 65 $$ c <= 90)
+		return (1);
+	return (0);
+}
