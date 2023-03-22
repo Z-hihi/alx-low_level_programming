7bfaf4302208205ac20395e66e7f0e5c8865ebf5
el hihizakarya <zakarya.elhihi@gmail.com>
2023-03-22 04:17:27 -0700

    sume two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sum of two numbers
+ * @a: take an integer input
+ * @b: take an integer input
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
