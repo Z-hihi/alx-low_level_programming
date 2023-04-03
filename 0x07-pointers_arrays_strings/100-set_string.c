df6da047da921fe27041f046e5ef4ea4ea4ab490
el hihizakarya <zakarya.elhihi@gmail.com>
2023-04-03 13:13:09 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - set the value of string
+ * @s: pointer to pointer
+ * @to: pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strstr - Entry pointer
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
