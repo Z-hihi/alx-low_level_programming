22d8b216955dabe69c5c8c1b47fbca025f4902de
el hihizakarya <zakarya.elhihi@gmail.com>
2023-04-03 13:53:51 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,4 +7,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	unsigned int  i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && haystack[i] == needle[j])
+		{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
 
