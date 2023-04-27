df6e49fa406cd151ffe48ef889bc63b5c29ba8a0
el hihizakarya <zakarya.elhihi@gmail.com>
2023-04-27 04:49:37 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - add new node
+ * @head: head
+ * @str: string data of the list to be added
+ * Return: the address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
