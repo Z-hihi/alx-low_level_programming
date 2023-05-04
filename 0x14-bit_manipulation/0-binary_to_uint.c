e14389b78a307b37d67379997cef2c9bc1f167f7
el hihizakarya <zakarya.elhihi@gmail.com>
2023-05-04 10:36:40 -0700

    binary to unisgned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to unsined
+ * @b: binary.
+ * Return: unsigned int.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k;
+	int ln, base_two;
+
+	if (!b)
+		return (0);
+
+	ui = 0;
+
+	for (ln = 0; b[ln] != '\0'; ln++)
+		;
+
+	for (ln--, base_two = 1; ln >= 0; ln--, base_two *= 2)
+	{
+		if (b[ln] != '0' && b[ln] != '1')
+		{
+			return (0);
+		}
+
+		if (b[ln] & 1)
+		{
+			k += base_two;
+		}
+	}
+
+	return (k);
+}
